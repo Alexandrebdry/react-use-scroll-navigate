dd1093acc7cfcf053acec9a825322f3764fc5b03
alexbaudry0 <alexbaudry0@gmail.com>
2022-11-05 21:50:14 +0100

    trying to fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import {useNavigate} from "react-router-dom";
-import {createContext, useCallback, useContext, useLayoutEffect, useMemo, useRef, useState} from "react";
-
+import React,{createContext, useCallback, useContext, useLayoutEffect, useMemo, useRef, useState} from "react";
 
 export class ScrollNavigatePathError extends Error {
     constructor(...params) {
@@ -22,37 +21,39 @@ export class ScrollNavigateTopError extends Error {
     }
 }
 
-/**
- *
- *  React hook to redirect to a new page and scroll to somewhere on the page
- *  It scrolls to the top by default
- *
- * @param {number} top - The y position to scroll to from top - default = 0 - must be between 0 and 100
- * @param {number} left - The x position to scroll to from left - default = 0 - must be between 0 and 100
- * @param {string} path - The path to navigate to - default = /
- */
-export function useScrollNavigate(path = "/", top = 0 , left = 0  ) {
+export default function useScrollNavigate() {
 
     const navigate = useNavigate();
-    const [scrollNavigateError, setScrollNavigateError] = useState(null) ;
-    const scrollNavigate = useCallback( (path, top, left) => {
+    const [scrollNavigateError, setScrollNavigateError] = useState(false) ;
+
+    /**
+     *  React hook to redirect to a new page and scroll to somewhere on the page
+     *  It scrolls to the top by default
+     *
+     * @param {number} top - The y position to scroll to from top - default = 0 - must be between 0 and 100
+     * @param {number} left - The x position to scroll to from left - default = 0 - must be between 0 and 100
+     * @param {string} path - The path to navigate to - default = /
+     */
+    const scrollNavigate = useCallback( (path = '/', top = 0, left = 0) => {
        if (typeof path != "string") {
            setScrollNavigateError(new ScrollNavigatePathError("Path is not a string")) ;
            return
        }
        if (typeof top !== "number") {
-           setScrollNavigateError(new ScrollNavigateTopError("Top is not a number"))
+           setScrollNavigateError(new ScrollNavigateTopError("Top is not a number"));
            return
        }
        if (top > 100 || top < 0) {
-           setScrollNavigateError(new ScrollNavigateLeftError("Top isn't between 0 and 100"))
+           setScrollNavigateError(new ScrollNavigateLeftError("Top isn't between 0 and 100"));
+           top = 0 ;
        }
        if(typeof left !== "number") {
-           setScrollNavigateError(new ScrollNavigateTopError("Left is not a number"))
+           setScrollNavigateError(new ScrollNavigateTopError("Left is not a number"));
            return
        }
         if (left > 100 || left < 0) {
-            setScrollNavigateError(new ScrollNavigateLeftError("Left isn't between 0 and 100"))
+            setScrollNavigateError(new ScrollNavigateLeftError("Left isn't between 0 and 100"));
+            left = 0 ;
         }
         setScrollNavigateError(false);
         navigate(path);
@@ -67,11 +68,13 @@ export function useScrollNavigate(path = "/", top = 0 , left = 0  ) {
     }
 }
 
+
 /** Scroll to a html ref */
-export const NavigateContext = createContext();
+const NavigateContext = createContext({});
 export const NavigateContextProvider = ({children}) => {
+
     const navigate = useNavigate();
-    const [navigationBeacon, setNavigationBeacon] = useState(null) ;
+    const [navigationBeacon, setNavigationBeacon] = useState(false) ;
     const navigationRef = useRef() ;
 
     const navigateToRef = useCallback((path) => {
@@ -80,11 +83,14 @@ export const NavigateContextProvider = ({children}) => {
 
     },[navigate, setNavigationBeacon]);
 
-    const value = useMemo(() => {navigationRef, navigateToRef},
+    const value = useMemo(() => {
+            navigationRef,
+            navigateToRef
+        },
         [navigationRef,navigateToRef]);
 
     useLayoutEffect(() => {
-       if (navigationBeacon === false || navigationBeacon === null )return
+       if (navigationBeacon === false )return
        if(navigationRef.current instanceof HTMLElement) {
            const {left,top} = navigationRef.current.getBoundingClientRect();
            window.scroll({left,top,behavior:'smooth'});
@@ -97,9 +103,7 @@ export const NavigateContextProvider = ({children}) => {
             {children}
         </NavigateContext.Provider>
     )
-}
-
-export const useNavigationContext = () => useContext(NavigateContext) ;
-
+};
+export const useRefNavigate = () => useContext(NavigateContext) ;
 
 
